18ea98aefffef1602c6a2a2795c756cb89db5609
kpi88 <raccoon@woods.koholint.is>
2024-03-07 03:52:55 -0500

    fixed kong url in sql migrations

--- dbmate-entrypoint.sh
+++ dbmate-entrypoint.sh
@@ -9,6 +9,6 @@ git clone --branch=main --depth=1 https://github.com/mckaywrigley/chatbot-ui;
 mkdir db && cp -r chatbot-ui/supabase/migrations /db/;
 for file in /db/migrations/*.sql; do sed -i -e '1s,^,-- migrate\:up\n,' "$file"; echo "-- migrate:down" >> "$file"; done;
 # https://github.com/mckaywrigley/chatbot-ui?tab=readme-ov-file#2-sql-setup
-sed -i -e "s,http://supabase_kong_chatbotui:8000,http://${CONTAINER_PREFIX}_kong_${CONTAINER_POSTFIX}:${KONG_HTTP_PORT}," \
+sed -i -e "s,http://supabase_kong_chatbotui:8000,${KONG_URL}," \
     -e "s/eyJ.*1IU/${SERVICE_KEY}/" /db/migrations/20240108234540_setup.sql
 /usr/local/bin/dbmate up;
